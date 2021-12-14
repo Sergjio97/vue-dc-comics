a2685bc37496f58ed12c0ba95b5175cf228b3874
Sergjio97 <sergiocizmja@gmail.com>
2021-12-14 18:58:58 +0100

    end header statico

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,19 @@
 <template>
-    <header></header>
+    <header>
+        <img src="../assets/img/dc-logo.png" alt="logo header">
+        <ul>
+            <li><a href="#">characters</a></li>
+            <li><a href="#">comics</a></li>
+            <li><a href="#">movies</a></li>
+            <li><a href="#">tv</a></li>
+            <li><a href="#">games</a></li>
+            <li><a href="#">collectibles</a></li>
+            <li><a href="#">videos</a></li>
+            <li><a href="#">fans</a></li>
+            <li><a href="#">news</a></li>
+            <li><a href="#">shop</a></li>
+        </ul>
+    </header>
 </template>
 
 <script>
@@ -9,5 +23,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/style/partials/variables.scss';
+
+    header {
+        width: 100vw;
+        height: 200px;
+        display: flex;
+        justify-content: space-around;
+        align-items: center; 
+    }
+
+    ul {
+        display: flex;
+        li {
+            list-style: none;
+            a {
+                text-decoration: none;
+                text-transform: uppercase;
+                padding: 50px 15px;
+                color: #464646;
+
+                &.active, &:hover {
+                    border-bottom: 4px solid $main-color;
+                }
+            }
+        }
+    }
 
 </style>
